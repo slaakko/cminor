6195b2f3278bbe7fd873e1a3f4a49d61d6f36c09
Seppo Laakko <slaakko@gmail.com>
2017-04-25 22:53:17 +0300

    install removed

--- install/builddebugsystem.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cminor clean system
-cminor build --native --link-with-debug-machine --use-ms-link system
-cminor clean --config=release system
-cminor build --config=release --native --link-with-debug-machine --use-ms-link system
--- install/buildreleasesystem.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cminor clean system
-cminor build --native --use-ms-link system
-cminor clean --config=release system
-cminor build --config=release --native --use-ms-link system
